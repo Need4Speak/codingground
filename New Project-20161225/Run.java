f03627394a1f9f628a95e66ab48fd1f9ba4cca86
Need4Speak <zcsd2668@163.com>
2016-12-25 18:36:07 +0800

    Created by Coding Ground Client

---
+++ New Project-20161225/Run.java
@@ -0,0 +1,6 @@
+public class Run {
+    public static void main() {
+        Pair<String> aPair = new Pair<>("a", "b");
+        System.out.println("aPair: " + aPair.getFirst() + ", " + aPair.getSecond());
+    }
+}
